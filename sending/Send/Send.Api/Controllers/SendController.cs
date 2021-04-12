462bd5effce29610a191e164f9ba7f40f4fd0e84
hedgehog <hedgehog.follower@gmail.com>
2021-04-12 18:25:40 +0200

    added sending solution

---
+++ sending/Send/Send.Api/Controllers/SendController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Send.Domain;
+
+namespace Send.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendController : ControllerBase
+    {
+        private readonly IReverser _reverser;
+
+        public SendController(IReverser reverser)
+        {
+            _reverser = reverser;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _reverser.Reverse(new[] { "Hi", "you", "call", "sending", "API" });
+        }
+    }
+}
